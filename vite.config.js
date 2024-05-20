0fc7ff181148eec3f2cbcfe193ea88b9fb440a3f
Godwin Chujor <jonahchujorgodwin@gmail.com>
2024-05-20 16:44:16 +0100

    Testing pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gcms-user/',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react()],
   server: {
     port: 3000,
